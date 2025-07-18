a43c70cd04278f7463fda35fdc64d2e6f96c4c51
corentin.loison <corentin.loison@emeria.eu>
2025-07-18 15:25:53 +0200

    feat(bun-server) initial commit hono roots

---
+++ server/routes/expenses.ts
@@ -0,0 +1,64 @@
+import { Hono } from 'hono';
+import { zValidator } from '@hono/zod-validator'
+import { z } from 'zod';
+
+const ExpenseSchema = z.object({
+    id: z.number().int().positive().min(1),
+    title: z.string().min(3).max(100),
+    amount: z.number().int().positive(),
+})
+
+type Expense = z.infer<typeof ExpenseSchema>
+
+const createExpenseSchema = ExpenseSchema.omit({ id: true })
+
+const makeFakeExpenses = (id: number, title: string, amount: number): Expense => ({
+    id,
+    title,
+    amount,
+})
+
+const fakeExpenses: Expense[] = [
+    makeFakeExpenses(1, 'Groceries', 50),
+    makeFakeExpenses(2, 'Utilities', 100),
+    makeFakeExpenses(3, 'Rent', 1200),
+]
+
+export const expensesRoutes = new Hono()
+    .get('/', (c) => {
+        return c.json({
+            expenses: [...fakeExpenses],
+        });
+    })
+    .post("/", zValidator("json", createExpenseSchema), (c) => {
+        const expense = c.req.valid("json")
+        fakeExpenses.push({ ...expense, id: fakeExpenses.length + 1 });
+        c.status(201);
+
+        return c.json(expense)
+    })
+    .get("/:id{[0-9]+}", (c) => {
+        const id = Number.parseInt(c.req.param('id'))
+        const expense = fakeExpenses.find(x => x.id === id)
+        if (!expense) {
+            return c.notFound()
+        }
+
+        return c.json({ expense })
+    })
+    .delete("/:id{[0-9]+}", (c) => {
+        const id = Number.parseInt(c.req.param('id'))
+        const index = fakeExpenses.findIndex(expense => expense.id === id)
+        if (index === -1) {
+            return c.notFound()
+        }
+
+        const deletedExpense = fakeExpenses.splice(index, 1)[0];
+
+        return c.json({ expense: deletedExpense })
+    })
+    .get('/total-spent', (c) => {
+        const total = fakeExpenses.reduce((sum, expense) => sum + expense.amount, 0);
+
+        return c.json({ total });
+    })
